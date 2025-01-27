87b3580cab0849e655c37f7b02504c089251623c
lugnut42 <lugnut42@ohnobono.com>
2025-01-27 11:09:55 -0800

    removed old update-imports.sh as this is no longer required

--- update_imports.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find . -name "*.go" -type f -exec sed -i.bak 's|"github.com/username/openipam/|"github.com/lugnut42/openipam/|g' {} +
-find . -name "*.go" -type f -exec sed -i.bak 's|"openipam/|"github.com/lugnut42/openipam/|g' {} +
-find . -name "*.go.bak" -type f -delete
